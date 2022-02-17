e3658edb224659fec4da897a9f36a0d98ac66c42
Lucas Dreher <lucasdreher@gmail.com>
2022-02-17 20:32:41 +0100

    Adding Styling

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Todo from '../models/todo';
 import TodoItem from './TodoItem';
+
+import classes from './Todos.module.css';
+
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
-	return <ul>{props.items.map((item) => <TodoItem key={item.id} text={item.text} />)}</ul>;
+	return <ul className={classes.todos}>{props.items.map((item) => <TodoItem key={item.id} text={item.text} />)}</ul>;
 };
 
 export default Todos;
